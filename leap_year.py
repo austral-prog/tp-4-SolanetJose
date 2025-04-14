0b4f2c2cf2ee8f24ef621356ae7dbc90078e5485
SolanetJose <jopytr123@duck.com>
2025-04-14 11:45:49 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,21 @@
 def leap_year():
-    print("TO DO")
+    text=input("Ingrese un año: ")
+    year=int(text)
+    resto=year%4
+    cent=year%100
+    cuat=year%400
+    
+    def first():
+        if resto==0:
+            second()
+        else:
+            print(f"El año {year} no es bisiesto")
+    
+    def second():
+        if cent==0 and cuat!=0:
+            print(f"El año {year} no es bisiesto")
+        elif cent==0 and cuat==0:
+            print(f"El año {year} es bisiesto")
+        else:
+            print(f"El año {year} es bisiesto")
+    first()
